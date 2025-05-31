536463c42e7db4b5879679504a404dcddc3c5b51
K ARYA SEKHAR DAS <karyasdas2005@gmail.com>
2025-05-31 12:32:44 +0530

    Update engine.py

--- core/engine.py
+++ core/engine.py
@@ -5,20 +5,13 @@ from core.interfaces import Cipher, Attack, AttackResult
 
 
 class CrypXEngine:
-    """
-    Orchestrates cryptanalysis by running attacks on ciphers.
-    """
-
     def __init__(self, cipher: Cipher):
         self.cipher = cipher
         self.attack_history = []
 
     def run_attack(self, attack: Type[Attack], **kwargs) -> AttackResult:
-        """
-        Runs a given attack on the current cipher.
-        """
         print(f"[+] Running {attack.__name__} on {self.cipher.__class__.__name__}")
-        attacker = attack(self.cipher)  # <-- FIXED: pass cipher to attack
+        attacker = attack(self.cipher)  
         result = attacker.run(**kwargs)
         self.attack_history.append((attack.__name__, result))
         return result
